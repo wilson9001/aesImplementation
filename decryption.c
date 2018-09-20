99c84987ce694504b5c23c55bbaf00041b285759
Alex Wilson <wilsonalexanderjohn@byu.net>
2018-09-19 22:52:05 -0600

    Began work on inverse cipher.

--- decryption.c
+++ decryption.c
@@ -1,3 +1,5 @@
+#include "decryption.h"
+
 void invShiftRows()
 {
 
@@ -18,12 +20,37 @@ void addRoundKey()
     
 }
 
-void invCipher(BYTE in[4*Nb], BYTE out[4*Nb], word *w, keyLength currentKeyLength)
+void invCipher(BYTE* in/*[4*Nb]*/, BYTE* out/*[4*Nb]*/, word* w, keyLength currentKeyLength)
 {
-    BYTE* state;
+    BYTE state[4][Nb];
 
-    state = in;
+    for (int row = 0; row < 4; row++)
+    {
+        for (int column = 0; column < Nb; column++)
+        {
+            state[row][column] = in[row + (4 * column)];
+        }
+    }
 
-    addRoundKey(state, w[Nr[currentKeyLength]*Nb][(Nr[currentKeyLength]+1)*(Nb -1)]);
+    addRoundKey(state, w[Nr[currentKeyLength] * Nb]);
 
+    for(int round = Nr[currentKeyLength] - 1; round < 1; round--)
+    {
+        invShiftRows(state);
+        invSubBytes(state);
+        addRoundKey(state, w[round * Nb]);
+        invMixColumns(state);
+    }
+
+    invShiftRows(state);
+    invSubBytes(state);
+    addRoundKey(state, w[0]);
+
+    for(int row = 0; row < 4; row++)
+    {
+        for(int column = 0; column < Nb; column++)
+        {
+            out[row + (4 * column)] = state[row][column];
+        }
+    }
 }
