fbe0c260c9faa341e7d582fdd66843752713aeab
Alex Wilson <wilsonalexanderjohn@byu.net>
2018-09-21 22:00:18 -0600

    Added and passed FIPS 256 Test

--- main.c
+++ main.c
@@ -308,6 +308,46 @@ int main(int argc, char const *argv[])
 
     invCipher(in, out, w1, KL192);
 
+    for(int i = 0; i < 16; i++)
+    {
+        in[i] = out[i];
+        out[i] = 0x00;
+    }
+
+    printf("\n\n~~~~~C.3~~~~~\nPLAINTEXT:\t\t");
+
+    for(int i = 0; i < 16; i++)
+    {
+        printf("%02x", in[i]);
+    }
+
+    BYTE key256[32] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f};
+
+    printf("\nKEY:\t\t\t");
+
+    for(int i = 0; i < 32; i++)
+    {
+        printf("%02x", key256[i]);
+    }
+
+    word w2[120];
+
+    keyExpansion(key256, w2, KL256);
+
+    printf("\n\nCIPHER (ENCRYPT):");
+
+    cipher(in, out, w2, KL256);
+
+    printf("\n\nINVERSE CIPHER (DECRYPT):");
+
+    for(int i = 0; i < 16; i++)
+    {
+        in[i] = out[i];
+        out[i] = 0x00;
+    }
+
+    invCipher(in, out, w2, KL256);
+
     printf("\n\n");
     return 0;
 }
